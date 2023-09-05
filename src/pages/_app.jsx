63c8920d8b6909cc4c363e0caa43b5c1ba61e472
Fahad Bhatti <fahadqayyumbhatti2001@gmail.com>
2023-09-06 01:27:23 +0500

    File Structure

---
+++ src/pages/_app.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react"
+import "@/styles/globals.css"
+
+export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const use = async () => {
+      await import("tw-elements")
+    }
+    use()
+  }, [])
+
+  return (
+    <Component {...pageProps} />
+  )
+}
